d86672024e0419ea161e240e34f6f0c5ebf5aa40
Jim Geovedi <jim@geovedi.com>
2022-02-14 14:15:27 +0700

    Update opt.py

--- bot/yoji-adx-breakout/opt.py
+++ bot/yoji-adx-breakout/opt.py
@@ -1,5 +1,6 @@
 import fire
 import os
+import random
 import sys
 import shutil
 import arrow
@@ -46,15 +47,25 @@ Jump 25 Index
 Jump 50 Index
 Jump 75 Index
 Jump 100 Index
-'''.strip().split("\n")
-
-'''
 Volatility 10 Index
 Volatility 25 Index
 Volatility 50 Index
 Volatility 75 Index
 Volatility 100 Index
-'''
+'''.strip().split("\n")
+
+DERIV_CRYPTO = '''
+BTCUSD ETHUSD
+BNBUSD BTCXAG BTCXAU 
+DSHUSD 
+EOSUSD 
+IOTUSD
+LTCUSD 
+NEOUSD 
+OMGUSD 
+XLMUSD XMRUSD XRPUSD 
+ZECUSD 
+'''.strip().split()
 
 SYMBOLS = DERIV_INDEX
 
@@ -126,7 +137,7 @@ ForwardMode=1
 Deposit=10000
 Currency=USD
 ProfitInPips=0
-Leverage=500
+Leverage=1000
 ExecutionMode=120
 OptimizationCriterion=6
 UseCloud=0
@@ -138,54 +149,53 @@ ShutdownTerminal=1
 [TesterInputs]
 ; Generic Setting
 _risk_type=0||0||0||2||N
-_risk_value=100||10.0||1.0||100.0||N
+_risk_value=100.0||10.0||1.0||100.0||N
 _expire=1
 ; Single Strategy Setting
 _symbol_name=__SYMBOL__
 _magic_number=__MAGIC__
 _session_start=22||0||2||22||Y
-_session_length=8||4||4||20||Y
-_lookback=15||5||5||20||Y
-_adx_tf=12||5||0||30||Y
-_adx_period=7||5||2||15||Y
+_session_length=8||4||4||12||Y
+_lookback=15||10||5||20||Y
+_adx_tf=15||5||0||16385||Y
+_adx_period=7||9||2||15||Y
 _adx_level=15||15||5||25||Y
-_atr_tf=5||5||0||30||Y
-_atr_period=9||5||2||15||Y
-_atr_factor=2||2.0||1.0||5.0||Y
-_rr_ratio=6||3.0||1.0||6.0||Y
+_atr_tf=15||5||0||16385||Y
+_atr_period=9||9||2||15||Y
+_atr_factor=2||1.0||1.0||5.0||Y
+_rr_ratio=6||1.0||1.0||6.0||Y
 ; Portfolio Strategy
 _config=__CONFIG_NAME__
 
 '''
 
 
-def read_xml(fname, magic, seq):
+def read_xml(fname, magic):
     df = xml2df(fname)
     print(f'Reading XML with {len(df)} entries...')
 
     if '_exit_mom' in df.columns:
         df['_exit_mom'] = df['_exit_mom'].apply(lambda x: True if x == 'true' else False)
 
-    df = df[(df['Forward Result'] > 0.5) & (df['Back Result'] > 0.5)]
-    df['profit'] = np.floor(df['Profit'] / 1000) * 1000
+    df = df[(df['Forward Result'] > 1.0) & (df['Back Result'] > 1.0)]
+    df['profit'] = np.floor(df['Profit'] / 2500) * 2500
     df = df[df['profit'] > 0]
     rescol = ['Forward Result', 'Back Result']
     df['score'] = df[rescol].std(axis=1)
-    #df['score'] = df['Back Result']
     df = df[(df['score']) < (df['score'].mean())]
     df = df.sort_values(['score'], ascending=(True))
-    #if seq > 3:
-    df = df.iloc[:20]
+    df = df.iloc[:30]
     df = df.sort_values(['profit', 'Back Result'], ascending=(False, False))
-    print(df.head())
+    if not df.empty:
+        print(df.head())
 
     if df.empty:
-        return None
+        return (None, 0.0)
 
     row = df.iloc[0].to_dict()
     rr_ratio = row.get('_rr_ratio', 3.0)
 
-    return (f'{row["_symbol"]},'
+    cfg = (f'{row["_symbol"]},'
             f'{magic},'
             f'{row["_session_start"]},'
             f'{row["_session_length"]},'
@@ -198,69 +208,63 @@ def read_xml(fname, magic, seq):
             f'{row["_atr_factor"]:.2f},'
             f'{rr_ratio:.2f}'
             )
+    return (cfg, row["profit"])
 
 
 def main(start_date="2021.04.01", end_date="2022.02.01", 
-         base_magic=100, max_seq=5, max_retry=3,
+         base_magic=100, max_cycle=3,
          run_config='run.ini', exp_name='exp'):
 
-    if not os.path.exists(f"{CONFIG_DIR}\\{exp_name}"):
-        os.makedirs(f"{CONFIG_DIR}\\{exp_name}")
 
-    try:
+    report_file = f"{exp_name}.xml"
+    config_file = f"{exp_name}.cfg"
+
+    with open(f"{CONFIG_DIR}\\{config_file}", "w") as out:
+        out.write(f"\n# {start_date} - {end_date}\n")
+
+    magic = base_magic + 1
+    profit = 0
+
+    for n_cycle in range(max_cycle):
+        random.shuffle(SYMBOLS)
+
         for symbol in SYMBOLS:
             reset_env()
 
-            report_file = f"{exp_name}_{symbol}.xml"
-            config_file = f"{exp_name}\\{symbol}.cfg"
+            print(f"\noptimisation started for {symbol}, magic={magic}, "
+                  f"min target={profit}")
 
-            with open(f"{CONFIG_DIR}\\{config_file}", "w") as out:
-                out.write(f"\n#\n")
+            try:
+                cfg = CONFIG
+                cfg = cfg.replace("__SYMBOL__", symbol)
+                cfg = cfg.replace("__START_DATE__", start_date)
+                cfg = cfg.replace("__END_DATE__", end_date)
+                cfg = cfg.replace("__MAGIC__", str(magic))
+                cfg = cfg.replace("__REPORT__", report_file)
+                cfg = cfg.replace("__CONFIG_NAME__", config_file)
+                
+                with open(run_config, 'w') as out:
+                    out.write(cfg)
 
-            for seq in range(1, max_seq + 1):
-                magic = base_magic + seq
-                print(
-                    f"optimisation started for {symbol}, magic={magic}"
+                cmd_out = check_output(
+                    f"{BIN_PATH}\\terminal64.exe /config:{run_config}"
                 )
+                (res, res_profit) = read_xml(f"{TERMINAL_DIR}\\reports\\"
+                                             f"{report_file}.forward.xml", 
+                                             magic)
 
-                #kill_tester()
-
-                res = None
-                n_trial = 0
-
-                while res is None:
-                    try:
-                        print(f"... trial={n_trial}\n")
-
-                        cfg = CONFIG
-                        cfg = cfg.replace("__SYMBOL__", symbol)
-                        cfg = cfg.replace("__START_DATE__", start_date)
-                        cfg = cfg.replace("__END_DATE__", end_date)
-                        cfg = cfg.replace("__MAGIC__", str(magic))
-                        cfg = cfg.replace("__REPORT__", report_file)
-                        cfg = cfg.replace("__CONFIG_NAME__", config_file)
-                        
-                        with open(run_config, 'w') as out:
-                            out.write(cfg)
-
-                        cmd_out = check_output(
-                            f"{BIN_PATH}\\terminal64.exe /config:{run_config}"
-                        )
-                        res = read_xml(f"{TERMINAL_DIR}\\reports\\{report_file}.forward.xml", 
-                                       magic, seq)
-                        if res is not None:
-                            break
-                        n_trial += 1
-                    except Exception as e:
-                        print(e)
-    
-                if res:
+                if res and res_profit > profit:
                     with open(f"{CONFIG_DIR}\\{config_file}", "a") as out:
                         out.write(f"{res}\n")
                     print(f"\nfound config: {res}\n\n")
+                    magic += 1
+                    profit = res_profit
 
-    except KeyboardInterrupt:
-        sys.exit(1)
+            except Exception as e:
+                print(e)
+
+            except KeyboardInterrupt:
+                sys.exit(1)
 
 
 if __name__ == '__main__':
