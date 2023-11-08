59e78d33af8fceaffbbe51978774c19454589c79
jim geovedi <jim@geovedi.com>
2023-11-08 02:37:39 +0000

    Update BearsPower.py

--- indicators/BearsPower.py
+++ indicators/BearsPower.py
@@ -1,36 +1,43 @@
 import pandas as pd
 import talib
 
-def BearsPower(dataframe, period=13):
+def BearsPower(dataframe, period=13, price='low'):
     """
     Calculate the Bears Power indicator.
 
-    Bears Power measures the balance of power between bears (sellers) and the bulls (buyers).
-    It is calculated as the difference between the lowest price and an Exponential Moving Average.
+    Bears Power evaluates the strength of sellers in the market by comparing the lowest
+    price with an Exponential Moving Average (EMA) of the lows.
 
     Args:
-    dataframe : pandas.DataFrame
+    dataframe: pandas.DataFrame
         A pandas DataFrame with 'high', 'low', 'open', 'close' and 'volume' columns.
-    period : int
-        The lookback period to calculate the Exponential Moving Average (EMA).
+    period: int
+        The period over which to calculate the EMA of the low prices.
+    price: str
+        The price type to use in the EMA calculation. Defaults to 'low' based on the
+        standard Bears Power indicator.
 
     Returns:
-    pandas.Series
-        A pandas Series containing the Bears Power indicator values.
+    pandas.Series:
+        A pandas Series with the Bears Power values.
     """
 
-    # Ensure that the DataFrame index is sequential (resetting index if needed)
-    df = dataframe.reset_index(drop=True)
+    if price not in ['high', 'low', 'open', 'close', 'median', 'typical', 'weighted']:
+        raise ValueError("Invalid price type. Allowed values are 'high', 'low', 'open', 'close', 'median', 'typical', 'weighted'.")
 
-    # Calculate the EMA on the 'low' price series
-    ema_low = talib.EMA(df['low'], timeperiod=period)
+    # Validate that the required 'low' column is present in the dataframe
+    if 'low' not in dataframe.columns:
+        raise ValueError("The input dataframe must contain a 'low' column.")
 
-    # Calculate Bears Power as the difference between the low prices and their EMA
-    bears = df['low'] - ema_low
+    # Calculate the EMA of the specified price series over the given period
+    ema_low = talib.EMA(dataframe[price], timeperiod=period)
 
-    # Return the Bears Power indicator series
+    # Compute the Bears Power as the difference between the low prices and the EMA of lows
+    bears = dataframe['low'] - ema_low
+
+    # Return the Bears Power series
     return bears
 
 # Example usage:
-# Assuming 'data' is a pandas DataFrame with the required columns
-# data['BearsPower'] = bears_power(data, period=13)
+# Assuming 'data' is a pandas DataFrame with the required columns:
+# data['BearsPower'] = BearsPower(data, period=13, price='low')
