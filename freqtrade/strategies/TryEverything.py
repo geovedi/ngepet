d4615304b4ebd39155a6bef1490d8d7f054f4773
jim geovedi <jim@geovedi.com>
2023-11-11 19:04:57 +0000

    Update TryEverything.py

--- freqtrade/strategies/TryEverything.py
+++ freqtrade/strategies/TryEverything.py
@@ -1,42 +1,22 @@
 import numpy as np
 import pandas as pd
 import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
-
 from functools import reduce
 from freqtrade.constants import Config
-from freqtrade.optimize.space import SKDecimal
+from freqtrade.optimize.space import SKDecimal, Dimension
 from freqtrade.strategy import CategoricalParameter, BooleanParameter
 from freqtrade.strategy.interface import IStrategy
-from pandas import DataFrame
+from freqtrade.vendor.qtpylib.indicators import crossed_above, crossed_below
+from pandas import DataFrame, Series
+from typing import Any, Dict, List
 
 
-def to_safe_numeric(series, fill_value):
+def to_numeric_safe(series: Series, fill_value: float) -> Series:
     return pd.to_numeric(series, errors="coerce").fillna(fill_value)
 
 
-def is_rising(series, n):
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("'n' must be a positive integer.")
-    if not pd.api.types.is_numeric_dtype(series):
-        raise TypeError("Series must contain numeric data.")
-
-    differences = series.diff()
-    is_rising = differences > 0
-    rising_streaks = is_rising.rolling(window=n).sum() == n
-    return rising_streaks.shift(-(n - 1)).fillna(False)
-
-
-def is_falling(series, n):
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("'n' must be a positive integer.")
-    if not pd.api.types.is_numeric_dtype(series):
-        raise TypeError("Series must contain numeric data.")
-
-    differences = series.diff()
-    is_falling = differences < 0
-    falling_streaks = is_falling.rolling(window=n).sum() == n
-    return falling_streaks.shift(-(n - 1)).fillna(False)
+is_rising = lambda s, n: (s.diff() > 0).rolling(n).sum() == n
+is_falling = lambda s, n: (s.diff() < 0).rolling(n).sum() == n
 
 
 class TryEverything(IStrategy):
@@ -48,79 +28,63 @@ class TryEverything(IStrategy):
     use_custom_stoploss = True
     startup_candle_count = 100
 
-    # Define a dictionary of parameter spaces for hyperopt optimization
     class HyperOpt:
         @staticmethod
-        def stoploss_space() -> list:
+        def stoploss_space() -> List[Dimension]:
             return [SKDecimal(-0.30, -0.01, decimals=2, name="stoploss")]
 
         @staticmethod
-        def roi_space() -> list:
+        def roi_space() -> List[Dimension]:
             return [SKDecimal(0.02, 0.30, decimals=2, name="roi")]
 
         @staticmethod
-        def generate_roi_table(params: dict) -> dict:
-            return {0: params["roi"]}
+        def generate_roi_table(p: Dict) -> Dict[int, float]:
+            return {0: p["roi"]}
 
-    # Initialize parameters for buying and selling
     def __init__(self, config: Config):
         super().__init__(config)
         self.config = config
-        self._initialize_parameters()
+        self.initialize_parameters()
 
-    def _initialize_parameters(self):
-        self._define_parameters_for_side("buy", "bband_upper")
-        self._define_parameters_for_side("sell", "bband_lower")
+    def initialize_parameters(self):
+        default_indicators = {"buy": "upperband", "sell": "lowerband"}
+        for side in default_indicators:
+            self.define_parameters_for_side(side, default_indicators[side])
 
-    def _define_parameters_for_side(self, side: str, default_indicator: str):
-        indicators = [
-            # Multiple output indicators
-            "upperband", "middleband", "lowerband",
-            "mama", "fama",
-            "aroondown", "aroonup",
-            "macd", "macdsignal", "macdhist",
-            "slowk", "slowd",
-            "fastk", "fastd",
-            # Single output indicators
-            "ad", "adosc", "adx", "adxr", "apo", "aroonosc", "atr", "bop", 
-            "cci", "cmo", "dema", "dx", "ema", "ht_trendline", "kama", "ma", 
-            "mfi", "minus_di", "minus_dm", "mom", "obv", "plus_di", "plus_dm", 
-            "ppo", "roc", "rocp", "rsi", "sar", "sarext", "sma", "tema", 
-            "trima", "trix", "ultosc", "willr", "wma",
-        ]
-        signal_ops = [
-            "disabled", "crossed_above", "crossed_below", "raising", "falling"
-        ]
-        price_type = [
-            "open", "high", "low", "close", 
-            "avgprice", "medprice", "typprice", "wclprice"
-        ]
-        indicator_shift = range(0, 10, 2)  # np.arange(0, 10, 2)
+    def create_categorical_parameter(self, name, categories, default, space):
+        setattr(self, name, CategoricalParameter(categories, default=default, space=space))
 
-        for i in range(TryEverything.RULES_SIZE):
-            self._create_categorical_parameter(f"{side}_indicator_{i}",
-                                               indicators, default_indicator,
-                                               side)
-            self._create_categorical_parameter(f"{side}_signal_ops_{i}",
-                                               signal_ops, "disabled", side)
-            self._create_categorical_parameter(f"{side}_indicator_shift_{i}",
-                                               indicator_shift, 0, side)
-            self._create_categorical_parameter(f"{side}_price_type_{i}",
-                                               price_type, "close", side)
-            self._create_boolean_parameter(f"{side}_use_price_{i}", False,
-                                           side)
-
-    def _create_categorical_parameter(self, name, categories, default, space):
-        setattr(self, name,
-                CategoricalParameter(categories, default=default, space=space))
-
-    def _create_boolean_parameter(self, name, default, space):
+    def create_boolean_parameter(self, name, default, space):
         setattr(self, name, BooleanParameter(default=default, space=space))
 
-    def populate_indicators(self, dataframe: DataFrame,
-                            metadata: dict) -> DataFrame:
-        # Multiple-output indicators
-        multi_output_indicators = {
+    def define_parameters_for_side(self, side: str, default_indicator: str):
+        indicators = (
+            "upperband middleband lowerband mama fama aroondown aroonup "
+            "macd macdsignal macdhist slowk slowd fastk fastd ad adosc "
+            "adx adxr apo aroonosc atr bop cci cmo dema dx ema "
+            "ht_trendline kama ma mfi minus_di minus_dm mom obv "
+            "plus_di plus_dm ppo roc rocp rsi sar sarext sma tema trima "
+            "trix ultosc willr wma"
+        ).split()
+        operation_types = "disabled crossed_above crossed_below raising falling".split()
+        price_types = "open high low close avgprice medprice typprice wclprice".split()
+        shifts = range(0, 10, 2)  # Shifts range
+
+        for rule_index in range(self.RULES_SIZE):
+            self.create_categorical_parameter(
+                f"{side}_indicator_{rule_index}", indicators, default_indicator, side
+            )
+            self.create_categorical_parameter(
+                f"{side}_operator_{rule_index}", operation_types, "disabled", side
+            )
+            self.create_categorical_parameter(f"{side}_shift_{rule_index}", shifts, 0, side)
+            self.create_categorical_parameter(
+                f"{side}_pricetype_{rule_index}", price_types, "close", side
+            )
+            self.create_boolean_parameter(f"{side}_useprice_{rule_index}", False, side)
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        multiple_output_indicators = {
             "BBANDS": ("upperband", "middleband", "lowerband"),
             "MAMA": ("mama", "fama"),
             "AROON": ("aroondown", "aroonup"),
@@ -128,103 +92,64 @@ class TryEverything(IStrategy):
             "STOCH": ("slowk", "slowd"),
             "STOCHF": ("fastk", "fastd"),
         }
+        single_output_indicators = (
+            "AD ADOSC ADX ADXR APO AROONOSC ATR BOP CCI CMO DEMA DX EMA "
+            "HT_TRENDLINE KAMA MA MFI MINUS_DI MINUS_DM MOM OBV "
+            "PLUS_DI PLUS_DM PPO ROC ROCP RSI SAR SAREXT SMA TEMA TRIMA "
+            "TRIX ULTOSC WILLR WMA AVGPRICE MEDPRICE TYPPRICE WCLPRICE"
+        ).split()
 
-        for indicator, column_names in multi_output_indicators.items():
-            results = getattr(ta, indicator)(dataframe)
-            for i, column_name in enumerate(column_names):
-                dataframe[column_name] = results[column_name]
+        for indicator_name, columns in multiple_output_indicators.items():
+            indicator_result = getattr(ta, indicator_name)(dataframe)
+            for column in columns:
+                dataframe[column] = indicator_result[column]
 
-        # Single-output indicators
-        single_output_indicators = [
-            "AD", "ADOSC", "ADX", "ADXR", "APO", "AROONOSC", "ATR", "BOP", 
-            "CCI", "CMO", "DEMA", "DX", "EMA", "HT_TRENDLINE", "KAMA", "MA", 
-            "MFI", "MINUS_DI", "MINUS_DM", "MOM", "OBV", "PLUS_DI", "PLUS_DM", 
-            "PPO", "ROC", "ROCP", "RSI", "SAR", "SAREXT", "SMA", "TEMA", 
-            "TRIMA", "TRIX", "ULTOSC", "WILLR", "WMA",
-            "AVGPRICE", "MEDPRICE", "TYPPRICE", "WCLPRICE",
-        ]
-
-        for func_name in single_output_indicators:
-            dataframe[func_name.lower()] = getattr(ta, func_name)(dataframe)
+        for indicator_name in single_output_indicators:
+            dataframe[indicator_name.lower()] = getattr(ta, indicator_name)(dataframe)
 
         return dataframe
 
-    def populate_entry_trend(self, dataframe: DataFrame,
-                             metadata: dict) -> DataFrame:
+    def populate_trend(self, dataframe: DataFrame, trend_side: str) -> DataFrame:
         conditions = []
 
-        for i in range(TryEverything.RULES_SIZE):
-            ops_val = getattr(self, f"buy_signal_ops_{i}").value
-            if ops_val != "disabled":
-                ind_val = getattr(self, f"buy_indicator_{i}").value
-                price_type = getattr(self, f"buy_price_type_{i}").value
-                shift_val = int(
-                    getattr(self, f"buy_indicator_shift_{i}").value)
-                use_price = getattr(self, f"buy_use_price_{i}").value
+        for rule_index in range(self.RULES_SIZE):
+            operator = getattr(self, f"{trend_side}_operator_{rule_index}").value
+            if operator == "disabled":
+                continue
 
-                left_val = dataframe[price_type] if use_price else dataframe[ind_val]
-                right_val = dataframe[ind_val].shift(shift_val)
+            indicator_name = getattr(self, f"{trend_side}_indicator_{rule_index}").value
+            shift_value = getattr(self, f"{trend_side}_shift_{rule_index}").value
+            use_price = getattr(self, f"{trend_side}_useprice_{rule_index}").value
+            price_type = getattr(self, f"{trend_side}_pricetype_{rule_index}").value
 
-                if ops_val == "crossed_above":
-                    conditions.append(
-                        qtpylib.crossed_above(
-                            to_safe_numeric(left_val, -np.inf),
-                            to_safe_numeric(right_val, -np.inf),
-                        ))
-                elif ops_val == "crossed_below":
-                    conditions.append(
-                        qtpylib.crossed_below(
-                            to_safe_numeric(left_val, np.inf),
-                            to_safe_numeric(right_val, np.inf),
-                        ))
-                elif ops_val == "raising":
-                    conditions.append(is_rising(left_val, shift_val + 1))
-                elif ops_val == "falling":
-                    conditions.append(is_falling(left_val, shift_val + 1))
+            series = dataframe[price_type] if use_price else dataframe[indicator_name]
+            shifted_series = series.shift(shift_value)
 
+            infinite_value = -np.inf if operator == "crossed_above" else np.inf
+            compare_function = crossed_above if operator == "crossed_above" else crossed_below
+            if operator in ["crossed_above", "crossed_below"]:
+                conditions.append(
+                    compare_function(
+                        to_numeric_safe(series, infinite_value),
+                        to_numeric_safe(shifted_series, infinite_value),
+                    )
+                )
+            elif operator == "raising":
+                conditions.append(is_rising(series, shift_value + 1))
+            elif operator == "falling":
+                conditions.append(is_falling(series, shift_value + 1))
+
+        conditions.append(dataframe["volume"] > 0)
+        return conditions
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = self.populate_trend(dataframe, "buy")
         if conditions:
-            conditions.append(dataframe["volume"] > 0)
-            dataframe.loc[reduce(lambda x, y: x & y, conditions),
-                          "enter_long"] = 1
-
+            dataframe.loc[reduce(lambda x, y: x & y, conditions), "enter_long"] = 1
         return dataframe
 
-    def populate_exit_trend(self, dataframe: DataFrame,
-                            metadata: dict) -> DataFrame:
-        conditions = []
-
-        for i in range(TryEverything.RULES_SIZE):
-            ops_val = getattr(self, f"sell_signal_ops_{i}").value
-            if ops_val != "disabled":
-                ind_val = getattr(self, f"sell_indicator_{i}").value
-                price_type = getattr(self, f"sell_price_type_{i}").value
-                shift_val = int(
-                    getattr(self, f"sell_indicator_shift_{i}").value)
-                use_price = getattr(self, f"sell_use_price_{i}").value
-
-                left_val = dataframe[price_type] if use_price else dataframe[ind_val]
-                right_val = dataframe[ind_val].shift(shift_val)
-
-                if ops_val == "crossed_above":
-                    conditions.append(
-                        qtpylib.crossed_above(
-                            to_safe_numeric(left_val, -np.inf),
-                            to_safe_numeric(right_val, -np.inf),
-                        ))
-                elif ops_val == "crossed_below":
-                    conditions.append(
-                        qtpylib.crossed_below(
-                            to_safe_numeric(left_val, np.inf),
-                            to_safe_numeric(right_val, np.inf),
-                        ))
-                elif ops_val == "raising":
-                    conditions.append(is_rising(left_val, shift_val + 1))
-                elif ops_val == "falling":
-                    conditions.append(is_falling(left_val, shift_val + 1))
-
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = self.populate_trend(dataframe, "sell")
         if conditions:
-            conditions.append(dataframe["volume"] > 0)
-            dataframe.loc[reduce(lambda x, y: x & y, conditions),
-                          "exit_long"] = 1
-
+            dataframe.loc[reduce(lambda x, y: x & y, conditions), "exit_long"] = 1
         return dataframe
