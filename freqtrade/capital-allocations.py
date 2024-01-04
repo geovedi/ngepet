9fc209a9e5ea1e9d6cca066524c37c6cbadec520
jim geovedi <jim@geovedi.com>
2024-01-04 09:11:38 +0700

    Update capital-allocations.py

--- freqtrade/capital-allocations.py
+++ freqtrade/capital-allocations.py
@@ -18,12 +18,14 @@ def autorename_columns(df):
     new_columns = list(map("-".join, unique_tokens))
     return df.rename(columns=dict(zip(df.columns, new_columns)))
 
+def round100(x):
+    return (x // 100) * 100
 
 def main(
     capital=5000.0,
     resample_mode="W",
-    rsi_period=5,
-    ema_period=5,
+    roc_period=4,
+    ema_period=8,
     user_dir="/home/ubuntu/freqtrade/user_data",
 ):
     df_list = list()
@@ -37,32 +39,28 @@ def main(
             columns={"close_date": "date", "profit_abs": strategy_name}
         )
         df.set_index("date", inplace=True)
+        df = df.resample("D").sum()
         df_list.append(df)
 
     df = pd.concat(df_list).resample(resample_mode).sum().cumsum()
     df.drop(df.tail(1).index, inplace=True)
+
     df = autorename_columns(df)
     df = df[sorted(df.columns)]
     print("\nReturns:")
-    print(df.tail(10))
+    print(df.tail(10).round(2))
 
-    df_rsi = df.apply(lambda x: ta.RSI(x, timeperiod=rsi_period))
-    print("\nReturns (RSI):")
-    print(df_rsi.tail(10))
-
-    df_rsi_ema = df_rsi.apply(lambda x: ta.EMA(x, timeperiod=ema_period))
-    print("\nReturns (RSI-EMA):")
-    print(df_rsi_ema.tail(10))
-
-    z = (df_rsi - df_rsi_ema).clip(0)
+    df_roc = df.apply(lambda x: ta.ROC(x, timeperiod=roc_period))
+    W = df_roc.apply(lambda x: ta.EMA(x, timeperiod=ema_period))
+    W = W.div(W.abs().sum(axis=1), axis=0)
     print("\nWeights:")
-    print(z.tail(10))
+    print(W.tail(10).round(2))
 
-    allocations = capital * z.div(z.abs().sum(axis=1), axis=0)
+    allocations = (capital * W).iloc[-1].apply(round100)
 
-    print("\nSuggested capital allocations:")
+    print(f"\nSuggested capital allocations: {allocations.sum()}")
     print(
-        rapidjson.dumps(allocations.iloc[-1].round(2).sort_index().to_dict(), indent=2)
+        rapidjson.dumps(allocations.sort_index().to_dict(), indent=2)
     )
     print()
 
