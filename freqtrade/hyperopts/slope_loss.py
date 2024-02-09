25eb20beb0c6a194c84a0f1f53c1608719fee708
jim geovedi <jim@geovedi.com>
2024-02-09 17:42:25 +0700

    Update slope_loss.py

--- freqtrade/hyperopts/slope_loss.py
+++ freqtrade/hyperopts/slope_loss.py
@@ -2,6 +2,8 @@ import numpy as np
 from pandas import DataFrame
 from freqtrade.optimize.hyperopt import IHyperOptLoss
 
+MAX_LOSS = 100000
+
 class SlopeLoss(IHyperOptLoss):
 
     @staticmethod
@@ -11,5 +13,5 @@ class SlopeLoss(IHyperOptLoss):
         trendline = np.linspace(returns.iloc[0], returns.iloc[-1], trade_count)
         slope = np.polyfit(returns, trendline, 1)[0]
         if returns.iloc[-1] < 0:
-            return 9999.0
+            return MAX_LOSS
         return -1.0 * slope
