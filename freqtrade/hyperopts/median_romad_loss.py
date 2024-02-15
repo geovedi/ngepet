78a3ac27d9643a2390d5a35e169fa92b578cae3d
jim geovedi <jim@geovedi.com>
2024-02-15 15:35:35 +0700

    Update median_romad_loss.py

--- freqtrade/hyperopts/median_romad_loss.py
+++ freqtrade/hyperopts/median_romad_loss.py
@@ -6,28 +6,31 @@ from freqtrade.data.metrics import calculate_max_drawdown
 
 MAX_LOSS = 100000
 
-class MedianReturnOverMaxDrawdownLoss(IHyperOptLoss):
 
+class MedianReturnOverMaxDrawdownLoss(IHyperOptLoss):
     @staticmethod
-    def hyperopt_loss_function(results: DataFrame, trade_count: int,
-                               min_date: datetime, max_date: datetime,
-                               *args, **kwargs) -> float:
+    def hyperopt_loss_function(
+        results: DataFrame, min_date: datetime, max_date: datetime, *args, **kwargs
+    ) -> float:
         scores = []
 
-        start_dates = date_range(start=min_date, end=max_date, freq='MS')
+        start_dates = date_range(start=min_date, end=max_date, freq="MS")
 
         for start_date in start_dates:
             end_date = start_date + DateOffset(months=3)
             if end_date > max_date:
                 break
 
-            # Filter results for the current 3-month chunk
-            chunk = results[(results.date >= start_date) & (results.date < end_date)]
+            chunk = results[
+                (results.open_date >= start_date) & (results.close_date < end_date)
+            ]
 
             if not chunk.empty:
                 total_profit = chunk["profit_abs"].sum()
                 try:
-                    max_drawdown = calculate_max_drawdown(chunk, value_col="profit_abs")[0]
+                    max_drawdown = calculate_max_drawdown(
+                        chunk, value_col="profit_abs"
+                    )[0]
                     scores.append(total_profit / max_drawdown)
                 except Exception:
                     scores.append(total_profit)
