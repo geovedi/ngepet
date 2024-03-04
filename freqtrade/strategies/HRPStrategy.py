2c98f154747b53479146c5826389c90f832ec51c
jim geovedi <jim@geovedi.com>
2024-03-04 16:13:45 +0700

    Update HRPStrategy.py

--- freqtrade/strategies/HRPStrategy.py
+++ freqtrade/strategies/HRPStrategy.py
@@ -26,22 +26,31 @@ class HRPStrategy(IStrategy):
     INTERFACE_VERSION: int = 3
     timeframe: str = "4h"
     can_short: bool = False
-    process_only_new_candles: bool = False
-    use_exit_signal: bool = False
+    process_only_new_candles: bool = True
+    use_exit_signal: bool = True
     ignore_buying_expired_candle_after: int = 600
     position_adjustment_enable: bool = True
-
+    startup_candle_count: int = 500
     minimal_roi: Dict[str, float] = {}
-    stoploss: float = -0.5
-    trailing_stop: bool = True
-    trailing_stop_positive: float = 0.05
-    trailing_stop_positive_offset: float = 0.2
-    trailing_only_offset_is_reached: bool = True
+    stoploss: float = -1.0
 
     entry_dayofweek = IntParameter(0, 6, default=0, space="buy")
     rebalance_candle = CategoricalParameter(range(50, 351, 50), default=100, space="buy")
     history_length = CategoricalParameter(range(100, 501, 100), default=200, space="buy")
     risk_measure = CategoricalParameter(RISK_MEASURES, default="MV", space="buy", optimize=False)
+    
+    custom_stop_loss = CategoricalParameter(
+        np.arange(-0.5, 0, 0.025).round(3),
+        default=-0.5, space="sell"
+    )
+    custom_trailing_stop = CategoricalParameter(
+        np.arange(0.025, 0.175, 0.025).round(3),
+        default=0.05, space="sell"
+    )
+    custom_trailing_offset = CategoricalParameter(
+        np.arange(0.125, 0.225, 0.025).round(3),
+        default=0.2, space="sell"
+    )
 
     def __init__(self, config: Config) -> None:
         super().__init__(config)
@@ -57,25 +66,6 @@ class HRPStrategy(IStrategy):
                     space="buy"
                 )
             )
-
-    class HyperOpt:
-        def stoploss_space() -> List[Dimension]:
-            stoploss_ranges = np.arange(-0.5, 0, 0.025).round(3)
-            return [Categorical(stoploss_ranges, name="stoploss")]
-        
-        def trailing_space() -> List[Dimension]:
-            stop_ranges = np.arange(0.025, 0.175, 0.025).round(3)
-            offset_ranges = np.arange(0.125, 0.225, 0.025).round(3)
-            return [
-                Categorical([True], name="trailing_stop"),
-                Categorical(stop_ranges, name="trailing_stop_positive"),
-                Categorical(offset_ranges, name="trailing_stop_positive_offset_p1"),
-                Categorical([True], name="trailing_only_offset_is_reached"),
-            ]
-
-    @property
-    def startup_candle_count(self):
-        return self.history_length.value
 
     @property
     def selected_pairs(self):
@@ -113,6 +103,38 @@ class HRPStrategy(IStrategy):
             return False
         return True
 
+    def custom_exit(self, pair: str, trade: "Trade", current_time: "datetime", current_rate: float,
+                    current_profit: float, **kwargs):
+        if not self.is_fresh_candle(current_time):
+            return None
+
+        dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
+        current_candle = dataframe.iloc[-1].squeeze()
+        current_rate = current_candle["close"]
+        current_profit = trade.calc_profit_ratio(current_rate)
+
+        if current_profit < self.custom_stop_loss.value:
+            logging.info(
+                f"{current_time} - custom_exit - {pair} - "
+                f"stop_loss with current_profit: {current_profit:.08f}"
+            )
+            return "custom_stop_loss"
+
+        max_rate = trade.min_rate if trade.is_short else trade.max_rate
+        max_profit = trade.calc_profit_ratio(max_rate)
+
+        if not max_profit > self.custom_trailing_offset.value:
+            return None
+
+        if (max_profit - current_profit) < self.custom_trailing_stop.value:
+            logging.info(
+                f"{current_time} - custom_exit - {pair} - "
+                f"trailing stop with current_profit: {current_profit:.08f}"
+            )
+            return "custom_trailing_stop"
+
+        return None
+
     def custom_stake_amount(self, pair: str, current_time: datetime, current_rate: float,
                             proposed_stake: float, min_stake: Optional[float], max_stake: float,
                             leverage: float, entry_tag: Optional[str], side: str,
@@ -125,7 +147,7 @@ class HRPStrategy(IStrategy):
         capital = self.wallets.get_total_stake_amount()
         stake_amount = pair_weight * capital
         logging.info(
-            f"{current_time} - custom_stake_amount - {pair} "
+            f"{current_time} - custom_stake_amount - {pair} - "
             f"pair_weight: {pair_weight:.03f}, "
             f"stake_amount: {stake_amount:.08f}")
         return stake_amount
