f871739fbd603e598e963c4906fe30803a9304a7
jim geovedi <jim@geovedi.com>
2024-05-01 16:24:51 +0700

    Update generate-pair-whitelist.py

--- freqtrade/tools/generate-pair-whitelist.py
+++ freqtrade/tools/generate-pair-whitelist.py
@@ -1,26 +1,28 @@
-
-
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import fire
 import rapidjson
 
-stake_currency = "USDT"
 
-sharpe = {}
-for fn in Path("user_data/data/binance").glob(f"*_{stake_currency}-1d.feather"):
-    df = pd.read_feather(fn)
-    df.set_index("date", inplace=True)
-    sharpe[fn.stem] = df["close"].pct_change().rolling(365).apply(lambda x: x.mean() / x.std() * np.sqrt(365))
+def main(stake_currency, input_config, output_config, data_dir="user_data/data/binance"):
+    sharpe = {}
+    for fn in Path(data_dir).glob(f"*_{stake_currency}-1d.feather"):
+        df = pd.read_feather(fn)
+        df.set_index("date", inplace=True)
+        sharpe[fn.stem] = df["close"].pct_change().rolling(365).apply(lambda x: x.mean() / x.std() * np.sqrt(365))
 
-df = pd.DataFrame(sharpe)
+    df = pd.DataFrame(sharpe)
 
-pairs = df[df.columns[(df.describe().T["count"] > 365 * 2) & (df.describe().T["50%"] > 0)]].describe().T.index.to_list()
-pairs = list(map(lambda x: f'{x.split("_")[0]}/{stake_currency}', pairs))
+    pairs = df[df.columns[(df.describe().T["count"] > 365 * 2) & (df.describe().T["50%"] > 0)]].describe().T.index.to_list()
+    pairs = list(map(lambda x: f'{x.split("_")[0]}/{stake_currency}', pairs))
+
+    config = rapidjson.load(open(input_config))
+    config["exchange"]["pair_whitelist"] = sorted(pairs)
+
+    with open(output_config, "w") as f:
+        f.write(rapidjson.dumps(config, indent=2) + "\n")
 
 
-config = rapidjson.load(open("config.json"))
-config["exchange"]["pair_whitelist"] = sorted(pairs)
-
-with open("config-sharpe.json", "w") as f:
-    f.write(rapidjson.dumps(config, indent=2) + "\n")
+if __name__ == '__main__':
+    fire.Fire(main)
